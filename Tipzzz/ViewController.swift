bf7d819e72d7318e154a73d4616771c16cb58d1f
Andy Patterson <andyp.dev@proton.me>
2025-02-27 17:44:14 -0600

    Initial Commit

---
+++ Tipzzz/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  Tipzzz
+//
+//  Created by apat on 2/27/25.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
